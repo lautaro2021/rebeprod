e5b047ed44b02f387039aafe5cc4b14a873092c4
lautaro2021 <lautaroarnay@gmail.com>
2024-02-25 12:42:41 -0300

    fix navbar bug

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,52 +7,57 @@ import { TLinkNav, linksNav } from "./linksNav";
 import { usePathname } from "next/navigation";
 
 export default function Navbar() {
-  const pathname = usePathname();
-  const [navOpen, setNavOpen] = useState(false);
+    const pathname = usePathname();
+    const [navOpen, setNavOpen] = useState(false);
 
-  return (
-    <header className={s.HeaderNavbar}>
-      <nav className={`container`}>
-        <div className={`smallContainer ${s.Navbar}`}>
-          <Link href="/">
-            <img
-              src="/assets/img/RebecaLogo.png"
-              alt="Rebeca Logo"
-              title="Rebeca ISO"
-            />
-          </Link>
-          <div
-            className={`${s.NavbarOptions} ${
-              navOpen ? s.NavbarOptionsMobileOpen : ""
-            }`}
-          >
-            {linksNav?.map((obj: TLinkNav) => {
-              const linkStyle = {
-                color: pathname === obj.url ? "#769E92" : "",
-              };
-              return (
-                <Link
-                  key={obj.text}
-                  href={obj.url}
-                  className="font-text"
-                  onClick={() => setNavOpen(false)}
-                  style={linkStyle}
-                >
-                  {obj.text}
-                </Link>
-              );
-            })}
-          </div>
-          <button
-            className={`${s.navBtnBurger} ${navOpen ? s.navBtnBurgerOpen : ""}`}
-            onClick={() => setNavOpen(!navOpen)}
-          >
-            <div className={s.navBtnLine}></div>
-            <div className={s.navBtnLine}></div>
-            <div className={s.navBtnLine}></div>
-          </button>
-        </div>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={s.HeaderNavbar}>
+            <nav className={`container`}>
+                <div className={`smallContainer ${s.Navbar}`}>
+                    <Link href="/">
+                        <img
+                            src="/assets/img/RebecaLogo.png"
+                            alt="Rebeca Logo"
+                            title="Rebeca ISO"
+                        />
+                    </Link>
+                    <button
+                        className={`${s.navBtnBurger} ${
+                            navOpen ? s.navBtnBurgerOpen : ""
+                        }`}
+                        onClick={() => setNavOpen(!navOpen)}
+                    >
+                        <div className={s.navBtnLine}></div>
+                        <div className={s.navBtnLine}></div>
+                        <div className={s.navBtnLine}></div>
+                    </button>
+                    {navOpen && (
+                        <div
+                            className={`${s.NavbarOptions} ${
+                                navOpen ? s.NavbarOptionsMobileOpen : ""
+                            }`}
+                        >
+                            {linksNav?.map((obj: TLinkNav) => {
+                                const linkStyle = {
+                                    color:
+                                        pathname === obj.url ? "#769E92" : "",
+                                };
+                                return (
+                                    <Link
+                                        key={obj.text}
+                                        href={obj.url}
+                                        className="font-text"
+                                        onClick={() => setNavOpen(false)}
+                                        style={linkStyle}
+                                    >
+                                        {obj.text}
+                                    </Link>
+                                );
+                            })}
+                        </div>
+                    )}
+                </div>
+            </nav>
+        </header>
+    );
 }
